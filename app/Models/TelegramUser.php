c4dafa167051de6a163e643034c6553961e3515c
nikymkv <nikymkv@gmail.com>
2021-06-14 22:04:21 +0300

    added fillable field to TelegramUser model

--- app/Models/TelegramUser.php
+++ app/Models/TelegramUser.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class TelegramUser extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'telegram_user_id',
+        'telegram_chat_id',
+    ];
 }
